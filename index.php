72be314b01a3f993fe6fafda9277285bbab37517
Aimar Etxenagusia Berriozabalgoitia <118171668+AimarEtxenagusia@users.noreply.github.com>
2025-09-25 13:14:47 +0200

    A

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
     <title>Hasiera</title>
     <link rel="stylesheet" href="css/login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-XXXX" crossorigin="anonymous">
-
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoA6VKHr8z2mbx5l1Z9gqG1skCkP0r5hXQ6tZTt3M1QF0k0" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
 </head>
 
@@ -23,7 +22,8 @@
         <input type="submit" value="SAIOA HASI">
         <input type="number" name="id" hidden>
     </form>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-YYYY"
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
         crossorigin="anonymous"></script>
 
 </body>
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
                 header("Location: bezeroa.php?id=" . $row['id']);
-                exit; 
+                exit;
 
             }
         } else {
