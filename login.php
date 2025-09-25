6661020451ce6c7718803f4fbbd17a8a0f3cd263
Aimar Etxenagusia Berriozabalgoitia <118171668+AimarEtxenagusia@users.noreply.github.com>
2025-09-25 10:27:40 +0200

    A

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+
+    require 'konexioa.php'
+
+?>
+
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hasiera</title>
+    <link rel="stylesheet" href="css/login.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <form action="login.php" method="post">
+        <label for="name" name="email">SARTU ZURE EMAIL-A</label>
+        <input type="email">
+    </form>
+
+</body>
+
+</html>
